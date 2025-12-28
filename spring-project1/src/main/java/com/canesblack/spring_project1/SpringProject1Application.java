80667f1b499eb076b54f6b556ad10060b9162004
LEE GEON U <leegeonu02@gmail.com>
2025-12-28 13:35:18 +0900

    251228_1

--- spring-project1/src/main/java/com/canesblack/spring_project1/SpringProject1Application.java
+++ spring-project1/src/main/java/com/canesblack/spring_project1/SpringProject1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringProject1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringProject1Application.class, args);
+        SpringApplication.run(SpringProject1Application.class, args);
 	}
 
 }
